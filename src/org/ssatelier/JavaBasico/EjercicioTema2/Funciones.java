a13f5bbb25a43867e28ca195ff96715be9812986
SebaSatelier <sebastian.satelier@gmail.com>
2023-01-23 15:57:35 -0300

    Agregado ejercicio Tema 2 Java Basico

---
+++ src/org/ssatelier/JavaBasico/EjercicioTema2/Funciones.java
@@ -0,0 +1,18 @@
+package org.ssatelier.JavaBasico.EjercicioTema2;
+
+public class Funciones {
+    public static void main(String[] args) {
+        double precioHeladera = 1300;
+
+        double precioNotebook = 1550.87;
+
+        System.out.println("Precio final Heladera = " + sumarIva(precioHeladera));
+        
+        System.out.println("Precio final Notebook = " + sumarIva(precioNotebook));
+
+
+    }
+    public static double sumarIva(double precio){
+        return precio * 1.21; // El IVA en Argentina es 21%
+    }
+}
